48c7e954573214b445c64a18bcd0090bcf2e51b5
manoel259 <manoel259@gmail.com>
2026-01-06 15:39:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Função para mostrar mensagem quando o botão é clicado
-function mostrarMensagem() {
-    const mensagemElement = document.getElementById('mensagem');
-    mensagemElement.textContent = '🎉 Parabéns! Você ativou o JavaScript!';
-}
 
-// Mensagem no console para desenvolvedores
-console.log('👋 Olá! O JavaScript foi carregado com sucesso!');
